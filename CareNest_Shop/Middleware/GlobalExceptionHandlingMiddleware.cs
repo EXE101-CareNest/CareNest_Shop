b098da861174c5c8f477b49bc6a5bdf759c334f7
Đông Nghi <hoangvodonghi@gmail.com>
2025-09-30 20:36:32 +0700

    Update GlobalExceptionHandlingMiddleware.cs

--- CareNest_Shop/Middleware/GlobalExceptionHandlingMiddleware.cs
+++ CareNest_Shop/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -28,48 +28,6 @@ namespace CareNest_Shop.Middleware
             }
         }
 
-        //private static Task HandleExceptionAsync(HttpContext context, Exception exception)
-        //{
-        //    string message;
-        //    int statusCode;
-        //    if (exception is BadRequestException)
-        //    {
-        //        statusCode = (int)HttpStatusCode.BadRequest;
-        //        message = exception.Message;
-        //    }
-        //    else if (exception is InternalException)
-        //    {
-        //        statusCode = (int)HttpStatusCode.InternalServerError;
-        //        message = exception.Message;
-        //    }
-        //    else
-        //    {
-        //        statusCode = (int)HttpStatusCode.InternalServerError;
-        //        message = "An unexpected error occurred.";
-        //    }
-
-        //    var response = new
-        //    {
-        //        error = new
-        //        {
-        //            message = exception.Message,
-        //            // có thể thêm stack trace khi dev:
-        //            // stackTrace = exception.StackTrace,
-        //            details = exception.InnerException?.Message
-        //        },
-        //        statusCode = statusCode,
-        //        timestamp = DateTime.UtcNow
-        //    };
-
-        //    string payload = JsonSerializer.Serialize(response);
-
-        //    context.Response.ContentType = "application/json";
-        //    context.Response.StatusCode = statusCode;
-
-        //    return context.Response.WriteAsync(payload);
-        //}
-
-        //BadRequest chuỗi, xuống dòng cho dễ xem
         private static Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
             int statusCode;
